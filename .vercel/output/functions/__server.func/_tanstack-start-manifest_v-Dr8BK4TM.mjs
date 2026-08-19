287c6d2c045f4120313caae0b9029870c85c932f
prabhuofficials <achuthanprabhukumar@gmail.com>
2026-08-19 17:48:54 +0530

    Updated for vercel

---
+++ .vercel/output/functions/__server.func/_tanstack-start-manifest_v-Dr8BK4TM.mjs
@@ -0,0 +1,20 @@
+//#region node_modules/.nitro/vite/services/ssr/assets/_tanstack-start-manifest_v-Dr8BK4TM.js
+var tsrStartManifest = () => ({ routes: {
+	__root__: {
+		filePath: "E:/code/Solar/aaryon-sun-spark/src/routes/__root.tsx",
+		children: ["/"],
+		preloads: ["/assets/index-DCwffZAg.js"],
+		scripts: [{ attrs: {
+			type: "module",
+			async: !0,
+			src: "/assets/index-DCwffZAg.js"
+		} }]
+	},
+	"/": {
+		filePath: "E:/code/Solar/aaryon-sun-spark/src/routes/index.tsx",
+		children: void 0,
+		preloads: ["/assets/routes-BV7KHfDw.js"]
+	}
+} });
+//#endregion
+export { tsrStartManifest };
