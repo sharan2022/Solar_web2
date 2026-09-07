56cfff825039a7f0672f50faba952632358d6f74
prabhuofficials <achuthanprabhukumar@gmail.com>
2026-09-07 11:41:49 +0530

    update the changes for vercel

---
+++ .vercel/output/functions/__server.func/_tanstack-start-manifest_v-DErwcsGh.mjs
@@ -0,0 +1,42 @@
+//#region node_modules/.nitro/vite/services/ssr/assets/_tanstack-start-manifest_v-DErwcsGh.js
+var tsrStartManifest = () => ({ routes: {
+	__root__: {
+		filePath: "E:/code/Solar/aaryon-sun-spark/src/routes/__root.tsx",
+		children: [
+			"/",
+			"/solar-calculator",
+			"/admin/solar-leads"
+		],
+		preloads: ["/assets/index-BJZUCG5h.js"],
+		scripts: [{ attrs: {
+			type: "module",
+			async: !0,
+			src: "/assets/index-BJZUCG5h.js"
+		} }]
+	},
+	"/": {
+		filePath: "E:/code/Solar/aaryon-sun-spark/src/routes/index.tsx",
+		children: void 0,
+		preloads: ["/assets/routes-7MwK2wxt.js", "/assets/Nav-CsHEa45v.js"]
+	},
+	"/solar-calculator": {
+		filePath: "E:/code/Solar/aaryon-sun-spark/src/routes/solar-calculator.tsx",
+		children: void 0,
+		preloads: [
+			"/assets/solar-calculator-BFjrOUaj.js",
+			"/assets/solar-calculations-DkpWtet9.js",
+			"/assets/Nav-CsHEa45v.js"
+		]
+	},
+	"/admin/solar-leads": {
+		filePath: "E:/code/Solar/aaryon-sun-spark/src/routes/admin.solar-leads.tsx",
+		children: void 0,
+		preloads: [
+			"/assets/admin.solar-leads-C2Xgr6Ep.js",
+			"/assets/solar-calculations-DkpWtet9.js",
+			"/assets/Nav-CsHEa45v.js"
+		]
+	}
+} });
+//#endregion
+export { tsrStartManifest };
